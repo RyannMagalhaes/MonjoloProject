f3131a9e045cca2ec5d9a88157b4ac80bb3d7c9b
Carlos Cayke B Nakazone <carlos.nakazone@estudante.iftm.edu.br>
2023-06-24 02:22:53 -0300

    Implementação do mapa e ajustes na barra de navegação
    Tentei implementar a barra de navegação de uma forma que a medida que a tela fosse descendo, o menu e desaparecesse e quando o usuário rolar um pouco pra cima ela apareça novamente

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,60 @@
 import "../../css/header.css"
+import React, { useState, useEffect } from 'react';
 import logo from "../../images/logo.jpg"
 
-export default function Header() {
+// export default function Header() {
 
+//     return (
+//         <header className="header1">
+//             <nav>
+//                 <ul>
+//                     <li><a href="#diferencial">Diferencial</a></li>
+//                     <li><a href="#catalogo">Catálogo</a></li>
+//                     <li><a href="#quemsomos">Quem somos</a></li>
+//                     <li>
+//                         <img src={logo}alt="" />
+//                     </li>
+//                     <li><a href="#depoimentos">Depoimentos</a></li>
+//                     <li><a href="#compre">Compre</a></li>
+//                     <li><a href="#contato">Contato</a></li>
+//                 </ul>
+//             </nav>
+//         </header>
+//     )
+// }
+
+
+export default function Header() {
+    const [scrollPos, setScrollPos] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const isVisible = scrollPos > currentScrollPos || currentScrollPos === 0;
+        setScrollPos(currentScrollPos);
+        setIsVisible(isVisible);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, [scrollPos]);
+  
     return (
-        <header className="header1">
-            <nav>
-                <ul>
-                    <li><a href="#diferencial">Diferencial</a></li>
-                    <li><a href="#catalogo">Catálogo</a></li>
-                    <li><a href="#quemsomos">Quem somos</a></li>
-                    <li>
-                        <img src={logo}alt="" />
-                    </li>
-                    <li><a href="#depoimentos">Depoimentos</a></li>
-                    <li><a href="#compre">Compre</a></li>
-                    <li><a href="#contato">Contato</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+      <header className={`header1 ${isVisible ? '' : 'header-hidden'}`}>
+        <nav>
+          <ul>
+            <li><a href="#diferencial">Diferencial</a></li>
+            <li><a href="#catalogo">Catálogo</a></li>
+            <li><a href="#quemsomos">Quem somos</a></li>
+            <li>
+              <img src={logo} alt="" />
+            </li>
+            <li><a href="#depoimentos">Depoimentos</a></li>
+            <li><a href="#compre">Compre</a></li>
+            <li><a href="#contato">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
--- src/components/main/section_5mapa/Mapa.jsx
+++ src/components/main/section_5mapa/Mapa.jsx
@@ -1,12 +1,26 @@
 import TituloSubtitulo from "../commom/TituloSubtitulo";
 import titulosDescricoes from "../../../js/titulosDescricoesSecoes.js"
 import Brasil from "../commom/Brasil";
+import "../../../css/section5.css"
 
 export default function Mapa() {
     return (
-        <section class="sectionbege">
-            <TituloSubtitulo tit={titulosDescricoes["mapa"].titulo} desc={titulosDescricoes["mapa"].descricao}/>
-            <Brasil/>
+        <section class="sectionbege section5">
+            <div className="divMaior5">
+                <div className="tituloSub5">
+                    <TituloSubtitulo tit={titulosDescricoes["mapa"].titulo} desc={titulosDescricoes["mapa"].descricao} />
+                </div>
+                <div className="div5">
+                    <div className="txt5">
+                        <p>Nos encontre próximo de você.</p>
+                    </div>
+                    <div className="Brasil">
+                        <Brasil />
+
+                    </div>
+
+                </div>
+            </div>
         </section>
     )
 }
